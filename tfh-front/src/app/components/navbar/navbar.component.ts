759f91df6f6f107aa18b211d6257e995189b21b2
LavVadnagara <lavvadnagara@gmail.com>
2025-03-24 23:06:42 +0530

    2nd commit - set up front end

---
+++ tfh-front/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tfh-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
